c75398641d5146d38f793740c54e1fa6a3ea0e85
kumar narukullapati <kumarraja.9502@gmail.com>
2022-04-29 14:52:22 +0530

    better

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import logo13234 from './images/icons.png';
+import './App.css';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import HJYGYT from '../images/icons.png';
+import headerbg from '../images/headerbg.jpg';
+import building from '../images/build2.jpg';
+import build from '../images/build3.jpg';
+// import { Button } from '@mui/material';
+
+
+
+
+class HeaderComponent extends Component {
+    render() {
+        return (
+            <div>
+                <div className='header'>
+                    <div> logo</div>
+                    <div>
+                        <img src={HJYGYT} alt='image1' style={{ height: '30px', width: '40px' }} />
+                    </div>
+                </div>
+
+                <div className='headerone'>
+
+                    <div className='blocks'>
+                        <div className='block1'>
+                            <img src={headerbg} alt='building1' style={{ height: '180px', width: '100%', borderRadius: '10px' }} />
+                        </div>
+                        <div className='block2'>
+                            <img src={building} alt='building1' style={{ height: '180px', width: '100%', borderRadius: '10px', marginTop: '-30px' }} />
+                        </div>
+                        <div className='block3'>
+                            <img src={build} alt='building1' style={{ height: '200px', width: '100%', borderRadius: '10px', marginTop: '-30px' }} />
+                        </div>
+                        <div className='block4'>
+
+                            <p style={{ fontWeight: 'bold' }}> what we do<br />integrated  design <br /> practices  of<br /> architecture,<br /> landscape, <br />interiors </p>
+                            {/* <p> practices  of</p>
+                            <p>architecture,</p>
+                            <p>landscape,</p>
+                            <p>interiors </p> */}
+                            <button onClick={this.handleChange}> learn more</button>
+
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default HeaderComponent;
